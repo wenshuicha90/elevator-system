52a7b71631af0918a1b02d07b41d2b27466e59b9
qing ye <qing@hostalias.ddidev.utah.edu>
2019-04-14 23:25:58 -0600

    implement the logic of the elevator system- handle external request (feature 7)

--- src/src/ElevatorSystem.java
+++ src/src/ElevatorSystem.java
@@ -30,7 +30,42 @@ public class ElevatorSystem {
     }
 
     public boolean handleExternalRequest(ExternalRequest r) {
+        //check maitenance first:
+        checkMaintenance();
+        List<Elevator> idles = new ArrayList<>();
+        List<Elevator> ups = new ArrayList<>();
+        List<Elevator> downs = new ArrayList<>();
+
+
+        //feature 7:
+        //idle elevator in the current request level > occupied elevator pass the floor on the way
+        // > closest idle elevator >   other idle elevator
+        for (int i = 0; i < numofElevators; i ++) {
+            Elevator curElevator = elevators.get(i);
+            if (curElevator.getStatus() == Status.Idle()){
+                //idle elevator in the current request level
+                if (curElevator.getCurrentLevel() == r.level()) {
+                    curElevator.handleExternalRequest(r);
+                    return true;
+                }
+                else {
+                    idles.add(curElevator);
+                }
+            }
+        }
 
     }
 
+
+    public void checkMaintenance() {
+        List<Elevator> usable = new ArrayList<>();
+        for (int i = 0; i < elevators.size(); i ++) {
+            // elevator with trips > 100  can not be added to the usable list
+            if (elevators.get(i).getTrips() < 101) {
+                usable.add(elevators.get(i));
+            }
+
+        }
+        elevators = usable;
+    }
 }
